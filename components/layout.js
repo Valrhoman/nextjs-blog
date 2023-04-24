61b94e2b1e7772a65c1735a3cf3eb6197dafbd6d
Valrhoman <valrhomanferrer@gmail.com>
2023-04-24 15:10:41 +0800

    Added favorite videos links in crash course review blog

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Valrhoman Ferrer";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Blog";
 
 export default function Layout({ children, home }) {
   return (
